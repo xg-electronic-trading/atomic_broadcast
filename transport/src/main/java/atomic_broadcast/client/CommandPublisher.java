61c109fd1e9c96d83fe04e873e7a63e92801c318
feyyaz alam <feyyaz_alam@hotmail.co.uk>
2022-12-26 21:20:45 +0000

    client-transport-cmd-publication

--- transport/src/main/java/atomic_broadcast/client/CommandPublisher.java
+++ transport/src/main/java/atomic_broadcast/client/CommandPublisher.java
@@ -1,8 +1,11 @@
 package atomic_broadcast.client;
 
+import org.agrona.DirectBuffer;
+
 public interface CommandPublisher {
     boolean connectToCommandStream();
     boolean isPublicationConnected();
     boolean isPublicationClosed();
+    boolean send(DirectBuffer buffer, int offset, int length);
 
 }
